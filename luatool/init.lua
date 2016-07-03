8f504aca04b79d6a714bfc33adbcfe31c467ea3d
Eugeniy Mikhailov <evgmik@gmail.com>
2016-07-03 19:47:42 -0400

    put my AP data

--- luatool/init.lua
+++ luatool/init.lua
@@ -5,5 +5,7 @@ print('MAC: ',wifi.sta.getmac())
 print('chip: ',node.chipid())
 print('heap: ',node.heap())
 -- wifi config start
-wifi.sta.config("SSID","MasterPassword")
+wifi.sta.config("beamhome","C930F921D7F672A23C55638F3B")
 -- wifi config end
+
+dofile("main.lua")
