d524105c3fe0ba9d08c88c2aa06927865b768a3c
Eugeniy Mikhailov <evgmik@gmail.com>
2016-08-13 13:09:32 -0400

    modified main file to sync time and continiously run sensor

--- luatool/main.lua
+++ luatool/main.lua
@@ -1,3 +1,4 @@
+-- Connecting to the network
 tmr.alarm(0, 1000, 1, function()
    if wifi.sta.getip() == nil then
       print("Connecting to AP...")
@@ -6,3 +7,16 @@ tmr.alarm(0, 1000, 1, function()
       tmr.stop(0)
    end
 end)
+
+-- syncronize clock
+sntp.sync('192.168.2.17',
+  function(sec,usec,server)
+    print('sync', sec, usec, server)
+  end,
+  function()
+   print('failed!')
+  end
+)
+
+-- now we print DHT sensor data every 10 seconds
+tmr.alarm(0, 10000, tmr.ALARM_AUTO, function() dofile("read_dht.lua") end )
