a954dd8fabe8f66528ba8f3a3943dfcdf797e877
Eugeniy Mikhailov <evgmik@gmail.com>
2016-08-13 12:21:46 -0400

    commented out integer sensor processing

--- luatool/read_dht.lua
+++ luatool/read_dht.lua
@@ -2,12 +2,14 @@ pin=3
 status, temp, humi, temp_dec, humi_dec = dht.read(pin)
 if status == dht.OK then
     -- Integer firmware using this example
+    --[[
     print(string.format("DHT Temperature:%d.%03d;Humidity:%d.%03d\r\n",
           math.floor(temp),
           temp_dec,
           math.floor(humi),
           humi_dec
     ))
+    --]]
 
     -- Float firmware using this example
     print("DHT Temperature:"..temp..";".."Humidity:"..humi)
